d9a840d326835afef9d18fa1213edace4e7aca2a
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-10-16 14:40:55 +0200

    Add Environment and key bindings

---
+++ Interactions/Environment.swift
@@ -0,0 +1,8 @@
+//
+//  Environment.swift
+//  Interactions
+//
+//  Created by Leonard on 16.10.24.
+//
+
+import Foundation
---
+++ Interactions/KeyBindings.swift
@@ -0,0 +1,8 @@
+//
+//  KeyBindings.swift
+//  Interactions
+//
+//  Created by Leonard on 16.10.24.
+//
+
+import Foundation
