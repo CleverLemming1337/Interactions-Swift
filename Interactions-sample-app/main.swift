dcdfb4ccbfb5c3d1968ac6c42a59aa621c85f75e
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-10-17 16:46:53 +0200

    Update alert demo

--- Interactions-sample-app/main.swift
+++ Interactions-sample-app/main.swift
@@ -54,7 +54,7 @@ struct AlertDemo: Scene {
         Button(.cD, "Show alert") {
             showAlert.value = true
         }
-        Text("This text will be behind the alert.")
+        Text("\nThis text will be behind the alert.")
         Alert(title: "Alert title", text: "Here’s to the crazy ones. The misfits. The rebels. The troublemakers. The round pegs in the square holes. The ones who see things differently. They’re not fond of rules. And they have no respect for the status quo. You can quote them, disagree with them, glorify or vilify them. But the only thing you can’t do is ignore them. Because they change things. They push the human race forward. And while some may see them as the crazy ones, We see genius. Because the people who are crazy enough to think they can change the world, Are the ones who do.", level: .info, isPresented: showAlert)
         
     }
